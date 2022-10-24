b0d5d49cd6d58ecbfa21207781d633c5b7693cde
shemich <bazzzalex@mail.ru>
2022-10-24 16:21:48 +0300

    init project

---
+++ src/main/java/ru/shemich/mypastebin3000/MyPastebin3000Application.java
@@ -0,0 +1,13 @@
+package ru.shemich.mypastebin3000;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyPastebin3000Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyPastebin3000Application.class, args);
+	}
+
+}
---
+++ src/test/java/ru/shemich/mypastebin3000/MyPastebin3000ApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.shemich.mypastebin3000;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyPastebin3000ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
