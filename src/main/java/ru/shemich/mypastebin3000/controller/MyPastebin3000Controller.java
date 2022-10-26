bffb4d5524982dbfcb0a0da91444d8e38726833b
shemich <bazzzalex@mail.ru>
2022-10-26 15:06:54 +0300

    feat: add thymeleaf page

--- src/main/java/ru/shemich/mypastebin3000/controller/MyPastebin3000Controller.java
+++ src/main/java/ru/shemich/mypastebin3000/controller/MyPastebin3000Controller.java
@@ -4,16 +4,24 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 import ru.shemich.mypastebin3000.api.request.MyPastebin3000Request;
 import ru.shemich.mypastebin3000.model.Paste;
 import ru.shemich.mypastebin3000.service.MyPastebin3000Service;
 
-@RestController
+@Controller
 @RequiredArgsConstructor
 public class MyPastebin3000Controller {
 
     private final MyPastebin3000Service myPastebin3000Service;
+
+    @GetMapping()
+    public String createPasteForm(Model model) {
+        model.addAttribute("request", new MyPastebin3000Request());
+        return "paste-create";
+    }
 
     @GetMapping("/{hash}")
     public ResponseEntity<String> getByHash(@PathVariable String hash) {
@@ -25,12 +33,19 @@ public class MyPastebin3000Controller {
         }
     }
 
-    @PostMapping(consumes = "application/json")
+    @PostMapping()
+    public String createPaste(@ModelAttribute MyPastebin3000Request request, Model model) {
+        Paste paste = new Paste();
+        myPastebin3000Service.create(request, paste);
+        return "redirect:/" + paste.getHash();
+    }
+
+   /* @PostMapping(consumes = "application/json")
     public ResponseEntity<String> add(@RequestBody MyPastebin3000Request request) {
         Paste paste = new Paste();
         String url = myPastebin3000Service.create(request, paste);
         HttpHeaders httpHeaders = new HttpHeaders();
         httpHeaders.add("Location", paste.getHash());
         return new ResponseEntity<>(url, httpHeaders, HttpStatus.CREATED);
-    }
+    }*/
 }
